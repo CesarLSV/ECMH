a52af2bd98312b4ed3eff62d978929ac2b531959
CesarLSV <cesarlopez1909@gmail.com>
2018-05-11 08:15:33 -0600

    ecmh

--- ECMH/ECMH/ViewModels/StartViewModel.cs
+++ ECMH/ECMH/ViewModels/StartViewModel.cs
@@ -1,10 +1,13 @@
 ﻿namespace ECMH.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
+    using System;
     using System.ComponentModel;
     using System.Windows.Input;
     using Views;
     using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
 
     public class StartViewModel : BaseViewModel
 
@@ -55,13 +58,36 @@
 
         }
 
+
+
+
         #endregion
 
 
         #region Properties
         public bool IsEnabled { get; set; }
-        
+
         #endregion
+
+
+        //void OnImageNameTapped(object sender, EventArgs args)
+        //{
+        //    try
+        //    {
+                
+
+        //        //    MainViewModel.GetInstance().Start = new StartViewModel();
+        //        //await Application.Current.MainPage.Navigation.PushAsync(new MonicoPage());
+
+        //        //Code to execute on tapped event
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+
 
     }
 }
